fb39cba426c26edb5a42c74ee98a89d6e070a316
Grey Izilein <grey.izilein@gmail.com>
2026-02-10 15:19:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// Prompt generation logic will go here
+document.getElementById("projectForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Project details captured. Prompt generation will be added next.");
+});
